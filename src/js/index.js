c7f4a2acd1674e784307fc41a0be3e2e8fe6de9d
Jungincha <jungincha923@gmail.com>
2019-10-17 23:37:11 +0900

    shopping list

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
-import Recipe from './models/Recipe';
+
 
 /* Global state of the app
 - Search object
@@ -16,6 +19,8 @@ import Recipe from './models/Recipe';
 */
 
 const state = {};
+
+window.state = state;
 
 const controlSearch = async () => {
     //1) Get query from the view
@@ -112,6 +117,23 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+/*
+* List Controller
+*/
+
+const controlList = () => {
+    // Create new list IF there is none yet
+    if(!state.list) {
+        state.list = new List();
+    }
+    
+    // Add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    })
+}
+
 // Handling recipe button clicks
 
 elements.recipe.addEventListener('click', e => {
@@ -126,6 +148,23 @@ elements.recipe.addEventListener('click', e => {
         // Increase button is clicked
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
+    }else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        controlList();
     }
     
+});
+
+// Handling shopping button clicks
+
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemId;
+    console.log(id);
+    //Handle delete button
+    if(e.target.matches('.shopping__delete, .shopping__delete *')) {
+        // delete item in the list
+        state.list.deleteItem(id);
+        
+        // remove item from UI
+        listView.deleteItem(id);
+    }
 })
